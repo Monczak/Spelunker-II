98d13b11a539c197fad82f2e2b268c687badccc7
Monczak <szymon.jedrzejczak.sj@gmail.com>
2020-01-01 03:56:45 +0100

    Update commit

---
+++ Assets/Scripts/Managers/UIManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public static UIManager Instance;
+
+    public FadeoutController fadeoutController;
+
+    private void Awake()
+    {
+        if (Instance == null) Instance = this;
+        if (Instance != this) Destroy(gameObject);
+    }
+
+    private void Start()
+    {
+        LevelManager.Instance.OnLevelLoaded += FadeOnLevelLoaded;
+        LevelManager.Instance.OnLevelHorizontalTransition += FadeOnLevelExit;
+        LevelManager.Instance.OnLevelVerticalTransition += FadeOnLevelExit;
+    }
+
+    void FadeOnLevelExit()
+    {
+        fadeoutController.SetFade(true);
+    }
+
+    void FadeOnLevelLoaded()
+    {
+        fadeoutController.SetFade(false);
+    }
+}
---
+++ Assets/Scripts/UI/FadeoutController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeoutController : MonoBehaviour
+{
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void SetFade(bool fade)
+    {
+        animator.SetBool("Fadeout On", fade);
+    }
+}
